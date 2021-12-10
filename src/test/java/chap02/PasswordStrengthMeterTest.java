77a5825196803a4ebc4bd6acbfcf3aaf9598ddb8
jessy <jessy@willog.io>
2021-12-10 15:08:26 +0900

    Chapter 02

---
+++ src/test/java/chap02/PasswordStrengthMeterTest.java
@@ -0,0 +1,66 @@
+package chap02;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class PasswordStrengthMeterTest {
+    private PasswordStrengthMeter meter = new PasswordStrengthMeter();
+
+    @Test
+    void meetsAllCriteria_Then_Strong() {
+        assertStrength("ab12!@AB", PasswordStrength.STRONG);
+        assertStrength("abc1!Add", PasswordStrength.STRONG);
+    }
+
+    @Test
+    void meetsOtherCriteria_except_for_Length_Then_Normal() {
+        assertStrength("ab12!@A", PasswordStrength.NORMAL);
+    }
+
+    @Test
+    void meetsOtherCriteria_except_for_Number_Then_Normal() {
+        assertStrength("ab!@ABqwer", PasswordStrength.NORMAL);
+    }
+
+    @Test
+    void meetsOtherCriteria_except_for_Uppercase_Then_Normal() {
+        assertStrength("ab12!@df", PasswordStrength.NORMAL);
+    }
+
+    @Test
+    void meetsOnlyLengthCreteria_Then_Weak() {
+        assertStrength("abdefghi", PasswordStrength.WEAK);
+    }
+
+    @Test
+    void meetsOnlyNumCriteria_Then_Weak() {
+        assertStrength("12345", PasswordStrength.WEAK);
+    }
+
+    @Test
+    void meetsOnlyUppCriteria_Then_Weak() {
+        assertStrength("ABZEF", PasswordStrength.WEAK);
+    }
+
+    @Test
+    void meetsNoCriteria_Then_Weak() {
+        assertStrength("abc", PasswordStrength.WEAK);
+    }
+
+
+    @Test
+    void nullInput_Then_Invalid() {
+        assertStrength(null, PasswordStrength.INVALID);
+    }
+
+    @Test
+    void emptyInput_Then_Invalid() {
+        assertStrength("", PasswordStrength.INVALID);
+    }
+
+    private void assertStrength(String password, PasswordStrength expStr) {
+        PasswordStrength result = meter.meter(password);
+        assertEquals(expStr, result);
+    }
+}

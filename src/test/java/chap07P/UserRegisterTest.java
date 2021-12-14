ff9d24080e639c6387a56f5df59f4ad471a90dca
jessy <jessy@willog.io>
2021-12-14 17:26:14 +0900

    chap 07 대역 (Mockito 사용)

--- src/test/java/chap07P/UserRegisterTest.java
+++ src/test/java/chap07P/UserRegisterTest.java
@@ -3,26 +3,37 @@ package chap07P;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
+import static org.mockito.Mockito.mock;
 
 public class UserRegisterTest {
     private UserRegister userRegister;
-    private StubWeakPasswordChecker stubPasswordChecker = new StubWeakPasswordChecker();
+    private WeakPasswordChecker mockWeakPasswordChecker = mock(WeakPasswordChecker.class);
     private MemoryUserRepository fakeRepository = new MemoryUserRepository();
-    private SpyEmailNotifier spyEmailNotifier = new SpyEmailNotifier();
+    private EmailNotifier mockEmailNotifier = mock(EmailNotifier.class);
 
     @BeforeEach
     void setUp() {
-        userRegister = new UserRegister(stubPasswordChecker, fakeRepository, spyEmailNotifier);
+        userRegister = new UserRegister(mockWeakPasswordChecker, fakeRepository, mockEmailNotifier);
     }
 
     @DisplayName("약한 암호면 가입 실패")
     @Test
     void weakPassword() {
-        stubPasswordChecker.setWeak(true); // 암호가 약하다고 응답하도록 설정
-
+        given(mockWeakPasswordChecker.checkPasswordWeak("pw")).willReturn(true); // 암호가 약하다고 응답하도록 설정
         assertThrows(WeakPasswordException.class, () -> userRegister.register("id", "pw", "email"));
+    }
+
+    @DisplayName("회원 가입시 암호 검사 수행함")
+    @Test
+    void checkPassword() {
+        userRegister.register("id", "pw", "email");
+        then(mockWeakPasswordChecker).should().checkPasswordWeak(anyString());
     }
 
     @DisplayName("중복된 아이디면 가입 실패")
@@ -41,13 +52,13 @@ public class UserRegisterTest {
         assertEquals("email", savedUser.getEmail());
     }
 
-
-
     @DisplayName("가입하면 이메일을 전송함")
     @Test
     void whenRegisterThenSendMail() {
         userRegister.register("id", "pw", "email@email.com");
-        assertTrue(spyEmailNotifier.isCalled());
-        assertEquals("email@email.com", spyEmailNotifier.getEmail());
+        ArgumentCaptor<String> captor = ArgumentCaptor.forClass(String.class);
+        then(mockEmailNotifier).should().sendRegisterEmail(captor.capture());
+        String realEmail = captor.getValue();
+        assertEquals("email@email.com", realEmail);
     }
 }

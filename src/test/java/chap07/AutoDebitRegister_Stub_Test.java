41c1df6a226fd38a77639632c185b5d764bf1ea2
jessy <jessy@willog.io>
2021-12-15 11:21:45 +0900

    파일 위치 이동, 변수나 필드를 사용해서 기댓값 표현하지 않기

--- src/test/java/chap07/AutoDebitRegister_Stub_Test.java
+++ src/test/java/chap07/AutoDebitRegister_Stub_Test.java
@@ -15,6 +15,7 @@ public class AutoDebitRegister_Stub_Test {
     스파이(Spy) : 호출된 내역을 기록. 기록한 내용은 테스트 결과를 검증할 때 사용한다. 스텁이기도 하다.
     모의(Mock) : 기대한 대로 상호작용 하는지 검증한다. 기대한 대로 동작하지 않으면 익셉션을 발생할 수 있다. 모의 객체는 스텁이자 스파이도 된다.
      */
+    /* 변수나 필드를 사용해서 기대값 표현하지 말기 */
 
     private AutoDebitRegister register;
     private StubCardNumberValidator cardNumberValidator;
@@ -29,36 +30,27 @@ public class AutoDebitRegister_Stub_Test {
 
     @Test
     void invalidCard() {
-        String invalidNo = "111122223333";
-        cardNumberValidator.setInvalidNo(invalidNo);
-
+        cardNumberValidator.setInvalidNo("111122223333");
         AutoDebitReq req = new AutoDebitReq("user1", "111122223333");
         RegisterResult result = register.register(req);
-
         assertEquals(CardValidity.INVALID, result.getValidity());
     }
 
     @Test
     void theftCard() {
-        String theftNo = "111122223333";
-        cardNumberValidator.setTheftNo(theftNo);
-
+        cardNumberValidator.setTheftNo("111122223333");
         AutoDebitReq req = new AutoDebitReq("user1", "111122223333");
         RegisterResult result = register.register(req);
-
         assertEquals(CardValidity.THEFT, result.getValidity());
     }
 
     @Test
     void alreadyRegistered_InfoUpdated() {
-        String userName = "user1";
-        String originalCardNumber = "111222333444";
-        String changedCardNumber = "1234567789012";
-        repository.save(new AutoDebitInfo(userName, originalCardNumber, LocalDateTime.now()));
-        AutoDebitReq req = new AutoDebitReq(userName, changedCardNumber);
+        repository.save(new AutoDebitInfo("user1", "111222333444", LocalDateTime.now()));
+        AutoDebitReq req = new AutoDebitReq("user1", "1234567789012");
         RegisterResult result = this.register.register(req);
         assertTrue(result.isResult());
-        AutoDebitInfo saved = repository.findOne(userName);
-        assertEquals(changedCardNumber, saved.getCardNumber());
+        AutoDebitInfo saved = repository.findOne("user1");
+        assertEquals("1234567789012", saved.getCardNumber());
     }
 }
